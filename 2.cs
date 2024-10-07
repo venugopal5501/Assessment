e898637222b9daf094919970ce92b0ded551fcf6
venugopal5501 <venugopal14082002@gmail.com>
2024-10-07 18:29:14 +0530

    tcs

---
+++ 2.cs
@@ -0,0 +1,40 @@
+using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int num = Convert.ToInt32(Console.ReadLine());
+
+
+
+        int[] rem = new int[32];
+        int count = 0;
+        while (num > 0)
+        {
+            rem[count] = num % 2;
+            num = num / 2;
+            count++;
+        }
+        int[] dem = new int[count];
+
+        for (int i = count - 1; i >= 0; i--)
+        {
+            if (rem[i] == 0)
+            {
+                rem[i] = 1;
+                dem[i] = rem[i];
+            }
+            else
+            {
+                rem[i] = 0;
+                dem[i] = rem[i];
+            }
+            Console.Write(rem[i]);
+        }
+        Console.WriteLine();
+        for (int i = 0; i < dem.Length; i++)
+        {
+            Console.Write(dem[i]);
+        }
+    }
+}
--- Assessment/obj/Debug/net8.0/Assessment.AssemblyInfo.cs
+++ Assessment/obj/Debug/net8.0/Assessment.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assessment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4d124a28262fdb25b0ab20c95035607b5d952a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08e7367c5206c0d3c300ca8ca53ce6f3d87d22a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assessment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assessment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
